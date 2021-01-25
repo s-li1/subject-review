f4bffda30399d3a769e9069e309e50aa5581e2cc
Steven.J.Li <14027496@student.uts.edu.au>
2021-01-25 23:25:47 +1100

    added function to check if user signed in with uts email domain name

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -6,10 +6,18 @@ import { UserContext } from '../../Providers/UserProvider';
 
 export default function Posts({posts, onCreate}) {
     const user = useContext(UserContext);
+
+    const isSignedInWithUTSAccount = ()=> {
+        if(user !=null) {
+            const { email } = user;
+            return email.includes("@student.uts.edu.au");
+        }
+    }
+        
     return (
         <div>
             <section className = "posts">
-                {user ? <AddPost onCreate={onCreate}/> : <p>Sign in with your UTS email via Google to write a review</p>}
+                {(user && isSignedInWithUTSAccount()) ? <AddPost onCreate={onCreate}/> : user ? <p>Currently signed with <b>{user.email}</b>. <br/> Sign in with your UTS student email via Google to write a review <br/> eg.12345678@student.uts.edu.au</p> : <p>Sign in with your UTS student email via Google to write a review <br/> eg.12345678@student.uts.edu.au</p>}
                 <h1>Recent Reviews</h1>
                 {posts.map( post => <Post {...post} key={post.id} />)}
             </section>
